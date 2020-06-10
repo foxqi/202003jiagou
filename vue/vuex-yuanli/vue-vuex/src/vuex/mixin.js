e104478c8e0fa81679b70d97d7c15f7bd8aa492b
zhangyanqi <1067083094@qq.com>
2020-06-10 16:47:21 +0800

    1

--- vue/vuex-yuanli/vue-vuex/src/vuex/mixin.js
+++ vue/vuex-yuanli/vue-vuex/src/vuex/mixin.js
@@ -1,13 +1,13 @@
 const applyMixin=(Vue)=>{
     Vue.mixin({
-        beforeCreate:vuexInit
+        beforeCreate:vuexInit//vuex的初始化
     })
 }
 // 组件的创建过程  是先父后子
 function vuexInit() {
     // vue-router 是把属性定义到了根实例上，所有组件都能拿到这个根，通过根实例获取这个属性
     // vuex  给每个组件都定义一个$store属性  指向的是同一个人
-    const options =this.$options;
+    const options =this.$options;//拿到根实例的属性
     if(options.store){
         // 根实例
         this.$store=options.store;
